9cc233faa637937e8a9985edb980fb66a782463e
Percy Jove <percy00261@gmail.com>
2025-06-13 12:53:33 -0500

    crear navbar

---
+++ front-page.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Single post template file.
+ *
+ * @package TailPress
+ */
+
+get_header();
+?>
+
+<section>
+    <div class="carousel bg-amber-900 h-dvh">
+        <div class="">
+
+        </div>
+    </div>
+</section>
+<div class="">
+    
+</div>
+
+<?php
+get_footer();
