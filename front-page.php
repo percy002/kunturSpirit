9efe91070b2bdaf36900f6f77266d3f486896309
Percy Jove <percy00261@gmail.com>
2025-06-17 17:22:06 -0500

    agregar seccion de tours

--- front-page.php
+++ front-page.php
@@ -9,23 +9,214 @@ get_header();
 ?>
 
 <section>
-    <div class="carousel h-dvh bg-center bg-no-repeat" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/mapi.webp'); background-position: 0px -233.718px;
-    background-size: 100% 132.74%;">
+    <div class="carousel h-dvh bg-center bg-no-repeat" style="background: linear-gradient(180deg, rgba(65, 24, 13, 0.80) 0%, rgba(0, 0, 0, 0.00) 100%), url('<?php echo get_template_directory_uri(); ?>/assets/images/mapi.webp')  0px -233.718px / 100% 132.74% no-repeat;
 
-        <div class="flex h-[600px] pt-0 pr-[150px] pl-[150px] pb-[150px] flex-col justify-end gap-5 self-stretch">
-            <div class="text-white">
+Colors
+Hex
+Hex
+Background colors
+">
+
+        <div
+            class="flex h-full pt-0 pr-[150px] pl-[150px] pb-[150px] flex-col justify-end gap-5 self-stretch items-center">
+            <div class="text-white flex-col gap-2.5 items-center">
                 <h1 class="">Discover the magic of Machu Picchu - your adventure awaits!</h1>
-                <p class="text-lg font-medium">Kuntur Spirit will craft your perfect Machu Picchu adventure, connecting
+                <p class="text-lg font-medium text-center">Kuntur Spirit will craft your perfect Machu Picchu adventure,
+                    connecting
                     you
                     to the wonders of the Andes and Inca history.</p>
             </div>
+            <a href=""><span class="py-2 px-5 bg-primary text-white font-medium">mas información</span></a>
 
         </div>
     </div>
 
 </section>
 <section>
-    
+    <div class="container flex flex-col gap-5">
+        <h1>Kuntur Spirit</h1>
+        <p class="text-center">Kuntur Spirit, que significa «El Espíritu del Cóndor», es una agencia de viajes local que
+            ofrece una variedad
+            de servicios turísticos en todo Perú. Nuestro equipo ha diseñado tres rutas únicas de Turismo Espiritual
+            para brindarte una experiencia memorable que nutra tu bienestar interior y exterior. Somos una familia unida
+            de profesionales dedicados a investigar, valorar y preservar el rico patrimonio cultural y la sabiduría
+            transmitida por nuestros antepasados. Guiados por sus filosofías metafísicas holísticas, buscamos fomentar
+            el crecimiento físico, emocional y espiritual, tanto para nosotros como para quienes nos acompañan en sus
+            viajes. Como agencia de viajes 100% peruana con años de experiencia en el sector turístico, Kuntur Spirit se
+            enorgullece de ofrecer experiencias auténticas que resaltan la belleza y la espiritualidad del Perú.</p>
+    </div>
+</section>
+<!-- SECCION DE TOURS PERU -->
+<section>
+    <div class="container flex flex-col gap-5">
+        <h1>Viaje por Perú: ¡Experiencias inolvidables!</h1>
+        <p>
+            Descubrir Perú es embarcarse en una aventura llena de contrastes, historia milenaria y paisajes que quitan
+            el aliento. Desde la majestuosidad de Machu Picchu hasta la inmensidad del desierto de Paracas, cada rincón
+            del país ofrece una experiencia única e inolvidable. En este viaje, te llevaremos por los sabores de su
+            gastronomía, los misterios de sus antiguas civilizaciones y la calidez de su gente. Prepárate para explorar
+            un Perú que va mucho más allá de los destinos turísticos tradicionales.
+        </p>
+    </div>
+    <div class="pt-10 flex flex-col gap-10">
+        <div class="container flex gap-7 justify-center">
+            <!-- card de tours -->
+            <article class="flex flex-col items-center bg-white " stryle="flex: 1 1 0;">
+                <div class="">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/camino.jpg" alt="">
+                </div>
+                <!-- contenido tour -->
+                <div class="flex flex-col gap-1.5 items-center p-2.5 self-stretch">
+                    <!-- etiquetas -->
+                    <div class="flex justify-between py-0 px-2.5 flex-start w-full">
+                        <div class="flex items-center gap-1.5 text-sm">
+                            <span class="text-tertiary"><i class="fa-solid fa-clock"></i></span>
+                            <span class="text-light">13 días / 12 noches</span>
+                        </div>
+                        <div class="bg-tertiary flex items-center px-2.5 py-1 rounded-xl">
+                            <span class="text-sm text-white">GRUPAL / PRIVADO</span>
+                        </div>
+                    </div>
+                    <!-- contenido -->
+                    <div class="flex flex-col gap-1.5">
+                        <h1 class="text-2xl font-bold text-primary my-0">Perú Fantástico: Una experiencia completa</h1>
+                        <p>Este tour de 13 días lo llevará a través de los destinos más famosos de Perú, incluidos
+                            Paracas, Cusco, Machu Picchu, la Montaña de Colores y el Lago Titicaca, combinando aventura,
+                            cultura y paisajes impresionantes.</p>
+
+                    </div>
+                    <!-- actions -->
+                    <div class="w-full py-2.5 px-5 flex justify-between items-center">
+                        <!-- precio -->
+                        <div class="flex flex-col justify-center" style="flex: 1 0 0">
+                            <!-- oferta -->
+                            <div class="flex gap-1.5 text-xs">
+                                <span>Antes: </span>
+                                <span class="font-bold">US$ 1250</span>
+                            </div>
+                            <span>Desde:</span>
+                            <span class="font-bold">US$ 1200</span>
+                        </div>
+                        <!-- botones -->
+                        <div class="flex gap-1.5 text-white ">
+                            <a href="" class="py-2.5 px-2.5 bg-secondary"><span>Ver Itinerario</span></a>
+                            <a href="" class="py-2.5 px-2.5 bg-primary"><span>Reserva</span></a>
+                        </div>
+                    </div>
+
+                </div>
+            </article>
+            <article class="flex flex-col items-center bg-white " stryle="flex: 1 1 0;">
+                <div class="">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/camino.jpg" alt="">
+                </div>
+                <!-- contenido tour -->
+                <div class="flex flex-col gap-1.5 items-center p-2.5 self-stretch">
+                    <!-- etiquetas -->
+                    <div class="flex justify-between py-0 px-2.5 flex-start w-full">
+                        <div class="flex items-center gap-1.5 text-sm">
+                            <span class="text-tertiary"><i class="fa-solid fa-clock"></i></span>
+                            <span class="text-light">13 días / 12 noches</span>
+                        </div>
+                        <div class="bg-tertiary flex items-center px-2.5 py-1 rounded-xl">
+                            <span class="text-sm text-white">GRUPAL / PRIVADO</span>
+                        </div>
+                    </div>
+                    <!-- contenido -->
+                    <div class="flex flex-col gap-1.5">
+                        <h1 class="text-2xl font-bold text-primary my-0">Perú Fantástico: Una experiencia completa</h1>
+                        <p>Este tour de 13 días lo llevará a través de los destinos más famosos de Perú, incluidos
+                            Paracas, Cusco, Machu Picchu, la Montaña de Colores y el Lago Titicaca, combinando aventura,
+                            cultura y paisajes impresionantes.</p>
+
+                    </div>
+                    <!-- actions -->
+                    <div class="w-full py-2.5 px-5 flex justify-between items-center">
+                        <!-- precio -->
+                        <div class="flex flex-col justify-center" style="flex: 1 0 0">
+                            <!-- oferta -->
+                            <div class="flex gap-1.5 text-xs">
+                                <span>Antes: </span>
+                                <span class="font-bold">US$ 1250</span>
+                            </div>
+                            <span>Desde:</span>
+                            <span class="font-bold">US$ 1200</span>
+                        </div>
+                        <!-- botones -->
+                        <div class="flex gap-1.5 text-white ">
+                            <a href="" class="py-2.5 px-2.5 bg-secondary"><span>Ver Itinerario</span></a>
+                            <a href="" class="py-2.5 px-2.5 bg-primary"><span>Reserva</span></a>
+                        </div>
+                    </div>
+
+                </div>
+            </article>
+            <article class="flex flex-col items-center bg-white " stryle="flex: 1 1 0;">
+                <div class="">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/camino.jpg" alt="">
+                </div>
+                <!-- contenido tour -->
+                <div class="flex flex-col gap-1.5 items-center p-2.5 self-stretch">
+                    <!-- etiquetas -->
+                    <div class="flex justify-between py-0 px-2.5 flex-start w-full">
+                        <div class="flex items-center gap-1.5 text-sm">
+                            <span class="text-tertiary"><i class="fa-solid fa-clock"></i></span>
+                            <span class="text-light">13 días / 12 noches</span>
+                        </div>
+                        <div class="bg-tertiary flex items-center px-2.5 py-1 rounded-xl">
+                            <span class="text-sm text-white">GRUPAL / PRIVADO</span>
+                        </div>
+                    </div>
+                    <!-- contenido -->
+                    <div class="flex flex-col gap-1.5">
+                        <h1 class="text-2xl font-bold text-primary my-0">Perú Fantástico: Una experiencia completa</h1>
+                        <p>Este tour de 13 días lo llevará a través de los destinos más famosos de Perú, incluidos
+                            Paracas, Cusco, Machu Picchu, la Montaña de Colores y el Lago Titicaca, combinando aventura,
+                            cultura y paisajes impresionantes.</p>
+
+                    </div>
+                    <!-- actions -->
+                    <div class="w-full py-2.5 px-5 flex justify-between items-center">
+                        <!-- precio -->
+                        <div class="flex flex-col justify-center" style="flex: 1 0 0">
+                            <!-- oferta -->
+                            <div class="flex gap-1.5 text-xs">
+                                <span>Antes: </span>
+                                <span class="font-bold">US$ 1250</span>
+                            </div>
+                            <span>Desde:</span>
+                            <span class="font-bold">US$ 1200</span>
+                        </div>
+                        <!-- botones -->
+                        <div class="flex gap-1.5 text-white ">
+                            <a href="" class="py-2.5 px-2.5 bg-secondary"><span>Ver Itinerario</span></a>
+                            <a href="" class="py-2.5 px-2.5 bg-primary"><span>Reserva</span></a>
+                        </div>
+                    </div>
+
+                </div>
+            </article>
+
+
+
+        </div>
+        <div class="w-full flex justify-center">
+            <a href="" class="py-4 px-16 bg-primary text-white font-medium !no-underline"><span>VER MÁS
+                    EXPERIENCIAS</span></a>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="relative">
+
+        <div class="bg-transparent relative h-[600px]">
+            <img class="h-full" src="<?php echo get_template_directory_uri(); ?>/assets/images/laguna.png" alt="">
+        </div>
+        <div class="absolute top-0 left-0">
+            <p>"No nos dedicamos al negocio de los viajes. Nos dedicamos al negocio de los sueños."</p>
+            <a href="">Contáctenos</a>
+        </div>
+    </div>
 </section>
 <?php
 get_footer();
