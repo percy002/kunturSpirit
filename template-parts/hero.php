a92a0986fa4c971cbfc3f01d8afc5e19aaa65450
ElvisValle <160268603+ElvisValle@users.noreply.github.com>
2025-07-02 17:06:30 -0500

    cambio de texto, cambios de pading, card de tours de 1 dia

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -11,7 +11,7 @@ $heroType = $args['heroType'] ?? '';
     <div class="carousel h-dvh max-h-[700px]"
         style="background: linear-gradient(180deg, rgba(65, 24, 13, 0.80) 0%, rgba(0, 0, 0, 0.00) 100%), url('<?= esc_url($imagen_url) ?>') center bottom / cover no-repeat;">
 
-        <div class="flex h-full pt-0 px-2.5 md:pr-[150px] md:pl-[150px] flex-col justify-center gap-8">
+        <div class="flex h-full pt-0 px-2.5 md:pr-[100px] md:pl-[100px] flex-col justify-center gap-8">
             <div class="flex text-white flex-col gap-2.5  w-[500px] max-w-11/12">
                 <h1 class="text-start text-[<?php $heroType == "tour" ? '40px' : '64px' ?>] text-white"><?= $title ?>
                 </h1>
