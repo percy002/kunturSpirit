c8525b406a73e5f690c051d978bfc4298f30fb7f
Percy Jove <percy00261@gmail.com>
2025-07-08 15:54:02 -0500

    hacer que colaboradores entre 2 por fila en dispositivos pequeños

--- page-nosotros.php
+++ page-nosotros.php
@@ -30,8 +30,10 @@ $infoHeroTour = array(
     </div>
 </section>
 <section>
-    <div class="container flex justify-end h-[600px] md:pr-[100px] md:pl-[100px]" style="background: url('<?php echo get_template_directory_uri(); ?>/assets/images/saludo.avif') lightgray 50% / cover no-repeat;">
-        <div class="flex flex-col justify-center gap-2.5 px-10 w-[340px]" style="background: rgba(255, 255, 255, 0.80);">
+    <div class="container flex justify-end h-[600px] md:pr-[100px] md:pl-[100px]"
+        style="background: url('<?php echo get_template_directory_uri(); ?>/assets/images/saludo.avif') lightgray 50% / cover no-repeat;">
+        <div class="flex flex-col justify-center gap-2.5 px-10 w-[340px]"
+            style="background: rgba(255, 255, 255, 0.80);">
             <div class="">
                 <h2 class="text-center text-secondary text-2xl">Misión</h2>
                 <?= $mision ?>
@@ -43,40 +45,25 @@ $infoHeroTour = array(
         </div>
     </div>
 </section>
-<?php get_template_part( 'template-parts/porquenosotros' ) ?>
+<?php get_template_part('template-parts/porquenosotros') ?>
 
 <section>
     <div class="container flex flex-col gap-1.5 md:pr-[100px] md:pl-[100px]">
         <h1>Todos Nuestros Colaboradores</h1>
-        <p class="text-center">Conoce a todos nuestros colaboradores que hacen de tu viaje, la mejor experiencia de tu vida.</p>
+        <p class="text-center">Conoce a todos nuestros colaboradores que hacen de tu viaje, la mejor experiencia de tu
+            vida.</p>
         <div class="flex justify-center flex-wrap gap-5">
-            <div class="flex flex-col items-center pt-5">
-                <img class="rounded-[100px] w-[180px] h-[180px] object-cover" src="<?php echo get_template_directory_uri(); ?>/assets/images/retrato.webp" alt="">
-                <span class="text-primary text-2xl">Juan Perez</span>
-                <span class="text-base">web master</span>
-            </div>
-            <div class="flex flex-col items-center pt-5">
-                <img class="rounded-[100px] w-[180px] h-[180px] object-cover" src="<?php echo get_template_directory_uri(); ?>/assets/images/retrato.webp" alt="">
-                <span class="text-primary text-2xl">Juan Perez</span>
-                <span class="text-base">web master</span>
-            </div>
-            <div class="flex flex-col items-center pt-5">
-                <img class="rounded-[100px] w-[180px] h-[180px] object-cover" src="<?php echo get_template_directory_uri(); ?>/assets/images/retrato.webp" alt="">
-                <span class="text-primary text-2xl">Juan Perez</span>
-                <span class="text-base">web master</span>
-            </div>
-            <div class="flex flex-col items-center pt-5">
-                <img class="rounded-[100px] w-[180px] h-[180px] object-cover" src="<?php echo get_template_directory_uri(); ?>/assets/images/retrato.webp" alt="">
-                <span class="text-primary text-2xl">Juan Perez</span>
-                <span class="text-base">web master</span>
-            </div>
-            <div class="flex flex-col items-center pt-5">
-                <img class="rounded-[100px] w-[180px] h-[180px] object-cover" src="<?php echo get_template_directory_uri(); ?>/assets/images/retrato.webp" alt="">
-                <span class="text-primary text-2xl">Juan Perez</span>
-                <span class="text-base">web master</span>
-            </div>
+            <?php for ($i = 0; $i < 5; $i++): ?>
+                <div class="flex flex-col items-center pt-5">
+                    <img class="rounded-[100px] w-[140px] h-[140px] md:w-[180px] md:h-[180px] object-cover"
+                        src="<?php echo get_template_directory_uri(); ?>/assets/images/retrato.webp" alt="">
+                    <span class="text-primary text-2xl">Juan Perez</span>
+                    <span class="text-base">web master</span>
+                </div>
+
+            <?php endfor ?>s            
         </div>
     </div>
 </section>
-<?php get_template_part( 'template-parts/mensaje' ) ?>
+<?php get_template_part('template-parts/mensaje') ?>
 <?php get_footer(); ?>
