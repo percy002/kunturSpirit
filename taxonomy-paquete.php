b4ca69877745552b3fce8daab2e8c4cc704c519c
percy002 <percy00261@gmail.com>
2025-08-17 23:26:07 -0500

    corregir responsividad en vista de taxonomias

--- taxonomy-paquete.php
+++ taxonomy-paquete.php
@@ -129,22 +129,8 @@ get_header();
 </section>
 
 <!-- MENSAJE -->
-<section>
-    <div class=""
-        style="background: linear-gradient(0deg, rgba(65, 24, 13, 0.50) 0%, rgba(65, 24, 13, 0.50) 100%), url('<?php echo get_template_directory_uri(); ?>/assets/images/pallayponcho.webp') lightgray 0px -750.816px / 100% 790.241% no-repeat;">
-        <div class="container py-12 flex gap-12 justify-center items-center">
-            <div class="flex flex-col gap-2.5 text-white">
-                <span class="text-5xl font-medium text-center">Aventuras hechas a tu medida </span>
-                <span class="text-2xl font-normal text-center">Cada viaje con “Kuntur Spirit” es único.</span>
-            </div>
-            <div class="border-r border-white h-24"></div>
-            <div class="">
-                <a href="" class="bg-white text-primary py-4 px-12 font-semibold"><span>CONTACTANOS</span></a>
-            </div>
-        </div>
-    </div>
+<?php get_template_part('template-parts/mensaje'); ?>
 
-</section>
 
 <?php
 get_footer();
